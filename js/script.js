912cffb6ac3455dbf9d7e8e45ef00feef830d258
Michał Mietła <mrbroomchannel@gmail.com>
2022-08-03 17:39:43 +0200

    Add 'active' class to clicked link

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@
 
 function titleClickHandler(event) {
     console.log('Link was clicked!');
-    console.log(event);
+    const clickedElement = this;
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -12,9 +12,11 @@ function titleClickHandler(event) {
         activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
 
-    /* remove class 'active' from all articles */
+    clickedElement.classList.add('active');
+
+    /* [DONE] remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('.posts .post.active');
 
